9a816eb4f9a97da7faf8ae783593ab9a3e33ac31
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-11-03 15:34:39 +0500

    change serializers.py

--- app_user/serializers.py
+++ app_user/serializers.py
@@ -1,5 +1,11 @@
 from rest_framework import serializers
 
+from .models import *
+
 
 class RegisterSerializer(serializers.ModelSerializer):
-    pass
+    confirm_password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = UserModel
+        fields = ['username', 'phone_number', 'password', 'confirm_password']
