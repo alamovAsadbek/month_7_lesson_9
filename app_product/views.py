e6c48d8154d6fb7887ec219249a267a58ba82177
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-11-03 23:13:40 +0500

    change

--- app_product/views.py
+++ app_product/views.py
@@ -7,3 +7,8 @@ from .serializers import ProductSerializer
 class ProductViewForUser(generics.ListAPIView):
     serializer_class = ProductSerializer
     queryset = ProductModel.objects.all()
+
+    def get_queryset(self):
+        return ProductModel.objects.filter(
+            status=ProductStatus.active.value
+        )
