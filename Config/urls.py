94bc76c3f85aaf24b58c2e9d141cc61f0a1abfdd
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-11-03 23:16:34 +0500

    change base.urls.py

--- Config/urls.py
+++ Config/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
+from app_product import views as product_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('product/', product_views.ProductViewForUser.as_view(), name='product')
 ]
