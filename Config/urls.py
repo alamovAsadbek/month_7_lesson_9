59d434835c28041e3505d12833a119043f2a8d30
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-11-03 23:18:48 +0500

    change base urls.py

--- Config/urls.py
+++ Config/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-
-from app_product import views as product_views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('product/', product_views.ProductViewForUser.as_view(), name='product')
+    path('api/', include('common.urls'))
 ]
