512b74afdba14fb0ff2050a87bbcd0b38c316577
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-11-03 22:03:29 +0500

    change

--- app_product/models.py
+++ app_product/models.py
@@ -3,3 +3,9 @@ from django.db import models
 
 class ProductModel(models.Model):
     title = models.CharField(max_length=50)
+    description = models.TextField()
+    price = models.PositiveIntegerField()
+
+    class Meta:
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
