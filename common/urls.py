987236ec35b4e8c01d3c4218e1807539441e1ba7
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-11-04 00:10:12 +0500

    change

--- common/urls.py
+++ common/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 
+from app_comment import views as comment_views
 from app_product import views as product_views
 from app_user import views as user_views
 
@@ -7,5 +8,6 @@ urlpatterns = [
     path('register/', user_views.RegisterView.as_view()),
     path('login/', user_views.LoginView.as_view()),
     path('product/', product_views.ProductViewForUser.as_view()),
-    path('product-for-admin/', product_views.ProductViewForAdmin.as_view())
+    path('product-for-admin/', product_views.ProductViewForAdmin.as_view()),
+    path('comment/', comment_views.CommentView.as_view())
 ]
