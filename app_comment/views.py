b9bd3f4f35db8f9e5013d94157c6f58da686bff9
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-11-04 00:03:26 +0500

    change

--- app_comment/views.py
+++ app_comment/views.py
@@ -1,5 +1,23 @@
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+from .serializers import *
 
 
 class CommentView(generics.GenericAPIView):
     serializer_class = CommentSerializer
+    queryset = CommentModel.objects.all()
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        request.data['user'] = request.user
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(user=request.user)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def get(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
