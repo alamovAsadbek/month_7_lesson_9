df9332d22da477962f3eeb8378009edcfede3e35
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-11-03 22:11:36 +0500

    add ProductStatus class

--- app_product/models.py
+++ app_product/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
 
+class ProductStatus(models.TextChoices):
+    pass
+
+
 class ProductModel(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
