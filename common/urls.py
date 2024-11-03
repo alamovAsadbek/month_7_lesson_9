0b3d39eca8640350d68ca296304ea572c282de1e
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-11-03 23:36:55 +0500

    change

--- common/urls.py
+++ common/urls.py
@@ -4,7 +4,7 @@ from app_product import views as product_views
 from app_user import views as user_views
 
 urlpatterns = [
-    path('register/', user_views.RegisterView.as_view(), name='register'),
-    path('login/', user_views.LoginView.as_view(), name='login'),
-    path('product/', product_views.ProductViewForUser.as_view(), name='product_for_users')
+    path('register/', user_views.RegisterView.as_view()),
+    path('login/', user_views.LoginView.as_view()),
+    path('product/', product_views.ProductViewForUser.as_view())
 ]
