d43953f45d383d3950111fe6b423b7f34f2d117c
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-11-03 22:39:59 +0500

    change app_comment/models.py

--- app_comment/models.py
+++ app_comment/models.py
@@ -1,5 +1,20 @@
 from django.db import models
 
+from app_product.models import ProductModel
+from app_user.models import UserModel
+
 
 class CommentModel(models.Model):
-    pass
+    user = models.ForeignKey(UserModel, on_delete=models.CASCADE, related_name='user')
+    product = models.ForeignKey(ProductModel, on_delete=models.CASCADE, related_name='product')
+    comment = models.TextField()
+
+    created_at = models.TimeField(auto_now_add=True)
+    update_at = models.TimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Comment'
+        verbose_name_plural = 'Comments'
+
+    def __str__(self):
+        return self.comment
