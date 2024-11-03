838e3fe5b5e3057be83371b24a3b1679ced06b29
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-11-04 00:13:22 +0500

    change

--- app_comment/serializers.py
+++ app_comment/serializers.py
@@ -7,3 +7,10 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = CommentModel
         fields = '__all__'
+
+    def validate(self, data):
+        user = self.context['request'].user
+        comment_user = data.get('user')
+        if user != comment_user:
+            raise serializers.ValidationError("You can only comment on your own posts.")
+        return data
