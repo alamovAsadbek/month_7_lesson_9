2ca6ed6e0109b1e8074b06a8be5f822e7f64772a
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-11-03 15:36:06 +0500

    add validate function

--- app_user/serializers.py
+++ app_user/serializers.py
@@ -9,3 +9,12 @@ class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserModel
         fields = ['username', 'phone_number', 'password', 'confirm_password']
+
+    def validate(self, data):
+        password = data.get('password')
+        confirm_password = data.get('confirm_password')
+
+        if password != confirm_password:
+            raise serializers.ValidationError("Passwords do not match")
+
+        return data
