6c60ff2cd5be75f147823bd859b5605e38210775
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-11-03 23:22:10 +0500

    change urls.py

--- common/urls.py
+++ common/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from app_user import views as user_views
 
 urlpatterns = [
-    path('register/', user_views.RegisterView.as_view(), name='register')
+    path('register/', user_views.RegisterView.as_view(), name='register'),
+    path('login/', user_views.LoginView.as_view(), name='login')
 ]
