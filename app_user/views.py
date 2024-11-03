b4ec84e4de31080dc3575c4d983d2e1e918e0410
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-11-03 21:26:54 +0500

    change

--- app_user/views.py
+++ app_user/views.py
@@ -1,4 +1,6 @@
+from django.contrib.auth import authenticate
 from rest_framework import generics
+from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .serializers import *
@@ -11,3 +13,14 @@ class RegisterView(generics.CreateAPIView):
 
 class LoginView(APIView):
     serializer_class = LoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        username = serializer.validated_data['username']
+        password = serializer.validated_data['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            return Response({'token': user.auth_token.key})
+        else:
+            return Response({'error': 'Invalid credentials'}, status=401)
